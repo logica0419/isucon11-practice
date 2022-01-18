e0f457939c500c13c914295049343142e2ed317e
logica <84759675+logica0419@users.noreply.github.com>
2022-01-18 21:48:32 +0900

    added index

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS `isu_association_config`;
+
 DROP TABLE IF EXISTS `isu_condition`;
+
 DROP TABLE IF EXISTS `isu`;
+
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `isu` (
@@ -12,8 +15,9 @@ CREATE TABLE `isu` (
   `jia_user_id` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-   PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+  PRIMARY KEY(`jia_isu_uuid`, `jia_user_id`),
+  INDEX `character` (`character`),
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `isu_condition` (
   `id` bigint AUTO_INCREMENT,
@@ -23,15 +27,15 @@ CREATE TABLE `isu_condition` (
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+  PRIMARY KEY(`jia_isu_uuid`, `timestamp`),
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `isu_association_config` (
   `name` VARCHAR(255) PRIMARY KEY,
   `url` VARCHAR(255) NOT NULL UNIQUE
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
