c2a71b63a10c5ba8c8fcef2fdba9ff3d424ef54b
logica <84759675+logica0419@users.noreply.github.com>
2022-01-20 22:02:05 +0900

    fixed initializing

--- sql/2_Patch.sql
+++ sql/2_Patch.sql
@@ -1,2 +1,10 @@
 ALTER TABLE `isu_condition`
-ADD `condition_level` VARCHAR(10) NOT NULL DEFAULT 'info';
+ADD `condition_level` VARCHAR(10) NOT NULL DEFAULT 'warning';
+
+UPDATE `isu_condition`
+SET `condition_level` = 'info'
+WHERE `condition` = `is_dirty=false,is_overweight=false,is_broken=false`;
+
+UPDATE `isu_condition`
+SET `condition_level` = 'critical'
+WHERE `condition` = `is_dirty=true,is_overweight=true,is_broken=true`;
