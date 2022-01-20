3ff3dc0e963082fe03c7fa577adee13b29e76865
logica <84759675+logica0419@users.noreply.github.com>
2022-01-20 22:32:29 +0900

    set unique key

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS `isu`;
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `isu` (
-  `id` bigint DEFAULT 0 NOT NULL,
+  `id` bigint AUTO_INCREMENT UNIQUE,
   `jia_isu_uuid` CHAR(36) NOT NULL UNIQUE,
   `name` VARCHAR(255) NOT NULL,
   `image` LONGBLOB,
@@ -20,7 +20,7 @@ CREATE TABLE `isu` (
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `isu_condition` (
-  `id` bigint DEFAULT 0 NOT NULL,
+  `id` bigint AUTO_INCREMENT UNIQUE,
   `jia_isu_uuid` CHAR(36) NOT NULL,
   `timestamp` DATETIME NOT NULL,
   `is_sitting` TINYINT(1) NOT NULL,
